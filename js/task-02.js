dcf30cdcc3dc2dc620154e2ad73fc14280848723
Serhii Shemendiuk <96024675+SerShem@users.noreply.github.com>
2023-02-19 23:58:36 +0200

    task 1, 2 done. commit

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector(`#ingredients`);
+
+ingredients.forEach((ingredientsItems) => {
+	const ingredient = document.createElement(`li`);
+	ingredient.textContent = ingredientsItems;
+	ingredient.className = `item`;
+	ingredientsList.appendChild(ingredient);
+});
+
